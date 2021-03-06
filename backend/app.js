acb364002b052d5dc2e73d01a53032f22e8c8389
JustDancePC <jesusdavid268@outlook.com>
2021-03-06 19:45:54 -0400

    otra vez con cors

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,20 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8000;
 
+let whitelist = ['http://localhost']
+
 let pushWooshRoutes = require('./routers/pushWooshRoutes');
 
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost'
-  }));
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1 || !origin) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    }
+}));
 app.use(express.json());
 
 app.use('/notifications', pushWooshRoutes);
--- proyecto2/App.js
+++ proyecto2/App.js
@@ -67,7 +67,7 @@ const App: () => React$Node = () => {
       method: 'POST',
       body: {
         userId: success,
-        time: '19:39'
+        time: '19:43'
       },
       headers:{
         'Content-Type': 'application/json'
