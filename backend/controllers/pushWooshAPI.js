10778e56bd9281c17f1c7de4acbc8d5ebe5e2405
JustDancePC <jesusdavid268@outlook.com>
2021-03-06 19:11:55 -0400

    prueba notificaciones

--- backend/controllers/pushWooshAPI.js
+++ backend/controllers/pushWooshAPI.js
@@ -3,8 +3,9 @@ const PW = require('pushwoosh-client');
 
 const registerClient = (req, res) => {
     const userId = req.body.UserId;
+    const time = req.body.time;
     const PWClient = new PW(process.env.APP_CODE, process.env.API_KEY);
-    PWClient.sendMessage('IT WORKED!!!', userId, {send_date: '2021-03-06 18:59', timezone: 'America/Caracas'}, (error, response) => {
+    PWClient.sendMessage('IT WORKED!!!', userId, {send_date: `2021-03-06 ${time}`, timezone: 'America/Caracas'}, (error, response) => {
         if (error){
             console.log(error);
             res.status(200).json({title: 'Error', content: 'Not nice'});
