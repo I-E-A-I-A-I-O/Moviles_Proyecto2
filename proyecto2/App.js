a38406781013a71efaa44ec11c1e39bbe4d29bea
JustDancePC <jesusdavid268@outlook.com>
2021-03-06 19:48:12 -0400

    dotenv

--- proyecto2/App.js
+++ proyecto2/App.js
@@ -67,7 +67,7 @@ const App: () => React$Node = () => {
       method: 'POST',
       body: {
         userId: success,
-        time: '19:43'
+        time: '19:47'
       },
       headers:{
         'Content-Type': 'application/json'
