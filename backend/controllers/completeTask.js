d12665de41a508e7d18fbee9545c22dea32e484e
JustDancePC <jesusdavid268@outlook.com>
2021-03-10 08:25:06 -0400

    Ahora se añaden tareas a completadas una vez la notificacion es recibida por el usuario

---
+++ backend/controllers/completeTask.js
@@ -0,0 +1,21 @@
+const db = require('../helpers/database');
+
+const completeTask = async (req, res) => {
+    let taskId = req.params.taskId;
+    let client = await db.getClient();
+    let query = 'DELETE FROM current_tasks WHERE task_id = $1';
+    let params = [taskId];
+    try{
+        await client.query(query, params);
+        query = 'INSERT INTO completed_tasks(task_id) VALUES($1)';
+        await client.query(query, params);
+        res.status(200).json({title: 'Success', content: ''});
+    }catch(err){
+        console.error(err);
+        res.status(500).json({title: 'Error', content: ''});
+    }finally{   
+        client.release();
+    }
+}
+
+module.exports = {completeTask};
--- backend/routers/taskRoutes.js
+++ backend/routers/taskRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const {saveTask} = require('../controllers/saveTask');
+const {completeTask} = require('../controllers/completeTask');
 
 router.post('/', saveTask);
+router.put('/task/:taskId', completeTask);
 
 module.exports = router;
