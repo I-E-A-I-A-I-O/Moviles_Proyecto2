18881ab068c0bf50617c6b539b38e5ae902f0551
JustDancePC <jesusdavid268@outlook.com>
2021-03-10 10:05:37 -0400

    Endpoint para recibir las tareas sin completar del usuario

--- backend/routers/taskRoutes.js
+++ backend/routers/taskRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const {saveTask} = require('../controllers/saveTask');
 const {completeTask} = require('../controllers/completeTask');
+const {getCurrentTasks} = require('../controllers/getCurrentTasks');
 
 router.post('/', saveTask);
 router.put('/task/:taskId', completeTask);
+router.get('/', getCurrentTasks);
 
 module.exports = router;
