f8788d25aa44092b9aa0ec7662e0373200baf210
JustDancePC <jesusdavid268@outlook.com>
2021-03-17 18:55:35 -0400

    s

--- backend/helpers/database.js
+++ backend/helpers/database.js
@@ -2,6 +2,12 @@ const {Pool} = require('pg');
 
 const pool = new Pool({
     connectionString: process.env.PGCONNECTION_STRING,
+    ssl:{
+        rejectUnauthorized: false,
+    },
+    idleTimeoutMillis: 1000,
+    min: 0,
+    query_timeout: 5000
 });
 
 pool.on('error', (error, client) => {
